c609fac9456de65b0c9ca75fbee230e2369724ca
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2022-07-02 08:10:57 -0400

    new testing scenes

--- main.js
+++ main.js
@@ -3,29 +3,34 @@ let ctx = canvasEl.getContext("2d")
 
 // let world = new World([new Sphere(20,new Vector(0,0,30),{}),new Sphere(7,new Vector(10,5,10),{}),new Sphere(7,new Vector(-15,-10,16),{})])
  //let world = new World([
-     //new Sphere(3, new Vector(4,3,8),{reflectivity:{r:1,g:1,b:1},glow:{r:200,g:200,b:200}}), 
+     //new Sphere(3, new Vector(4,3,2),{reflectivity:{r:1,g:1,b:1},glow:{r:200,g:200,b:200}}), 
      //new Sphere(3, new Vector(-4,3,8),{reflectivity:{r:1,g:0.5,b:0.5},glow:{r:100,g:10,b:10}}),
      //new Sphere(3, new Vector(0,-3,8),{reflectivity:{r:1,g:0.5,b:0.5},glow:{r:100,g:10,b:10}})
  //])
+ let world = new World([
+     new Sphere(3, new Vector(4,-30,50),{reflectivity:{r:1,g:1,b:1},glow:{r:255,g:255,b:255}}), 
+     new Sphere(10, new Vector(-4,10,50),{reflectivity:{r:1,g:0.5,b:0.5},glow:{r:0,g:0,b:0}}),
+     new Sphere(1000, new Vector(0,1020,50),{reflectivity:{r:1,g:1,b:1},glow:{r:0,g:0,b:0}})
+ ])
  //let world = new World([
      //new Sphere(200, new Vector(0,205,50),{reflectivity:{r:1,g:1,b:1},glow:{r:100,g:100,b:100}}),
      //new Sphere(200, new Vector(0,-205,50),{reflectivity:{r:1,g:1,b:1},glow:{r:10,g:10,b:10}})
  //])
- let world = new World([
-     new Sphere(20, new Vector(0,0,50),{reflectivity:{r:1,g:0.8,b:0.4},glow:{r:50,g:25,b:10}}),
-     new Sphere(500, new Vector(0,-700,50),{reflectivity:{r:1,g:1,b:1},glow:{r:200,g:200,b:200}}),
-     new Sphere(500, new Vector(0,700,50),{reflectivity:{r:1,g:1,b:1},glow:{r:200,g:200,b:200}})
- ])
+ //let world = new World([
+     //new Sphere(30, new Vector(0,0,50),{reflectivity:{r:1,g:0.8,b:0.4},glow:{r:50,g:25,b:10}}),
+     //new Sphere(500, new Vector(0,-700,50),{reflectivity:{r:1,g:1,b:1},glow:{r:200,g:200,b:200}}),
+     //new Sphere(500, new Vector(0,700,50),{reflectivity:{r:1,g:1,b:1},glow:{r:200,g:200,b:200}})
+ //])
 
 
-width = 500
-height = 500
+width = 2000
+height = 2000
 tryBounce = true
 
 ctx.canvas.width = width
 ctx.canvas.height = height
 
-let fieldOfView = 10 * width / 100
+let fieldOfView = 50 * width / 100
 
 let farthestPoint = 0
 let closestPoint = fieldOfView
@@ -60,10 +65,11 @@ function trace(ray,depth,currentObj){
 }
 
 function reflect(point,ray,normal){
+    let roughness = 3
     let newDirection = Vector.subtract(ray.direction, Vector.scale(normal, 2 * Vector.dot(ray.direction,normal)))
-    newDirection.x += (Math.random()-0.5)/2
-    newDirection.y += (Math.random()-0.5)/2
-    newDirection.z += (Math.random()-0.5)/2
+    newDirection.x += (Math.random()-0.5)*roughness
+    newDirection.y += (Math.random()-0.5)*roughness
+    newDirection.z += (Math.random()-0.5)*roughness
     return(new Ray(point,newDirection))
 }
 
