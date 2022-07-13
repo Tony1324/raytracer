a909718428367e9fb1a796ec22f560698a9a9cce
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2022-07-12 22:45:39 -0400

    formatting and triangle work

--- main.js
+++ main.js
@@ -22,91 +22,96 @@ let ctx = canvasEl.getContext("2d")
 // new Sphere(500, new Vector(0,700,50),{reflectivity:{r:1,g:1,b:1},glow:{r:200,g:200,b:200}})
 // ])
 let world = new World([
-    new Triangle([new Vector(-3,1,10), new Vector(3,1,10), new Vector(-3,7,10)], {reflectivity:{r:1,g:0.5,b:1}, glow:{r:0,g:0,b:0}}),
-    new Triangle([new Vector(3,7,10), new Vector(-3,7,10), new Vector(3,1,10)], {reflectivity:{r:1,g:0.5,b:1}, glow:{r:0,g:0,b:0}}),
-    new Triangle([new Vector(-3,1,13), new Vector(3,1,13), new Vector(-3,7,13)], {reflectivity:{r:0.5,g:1,b:1}, glow:{r:0,g:0,b:0}}),
-    new Triangle([new Vector(3,7,13), new Vector(-3,7,13), new Vector(3,1,13)], {reflectivity:{r:0.5,g:1,b:1}, glow:{r:0,g:0,b:0}}),
-    new Triangle([new Vector(-3,1,10), new Vector(-3,1,13), new Vector(3,1,13)], {reflectivity:{r:1,g:1,b:0.5}, glow:{r:0,g:0,b:0}}),
-    new Triangle([new Vector(3,1,10), new Vector(-3,1,10), new Vector(3,1,13)], {reflectivity:{r:1,g:1,b:0.5}, glow:{r:0,g:0,b:0}}),
-    new Sphere(0.1, new Vector(4,0,7), {reflectivity:{r:1,g:1,b:1}, glow:{r:255,g:255,b:255}})
+    // new Triangle([new Vector(-3, 1, 10), new Vector(3, 1, 10), new Vector(-3, 7, 10)], { reflectivity: { r: 1, g: 0.5, b: 1 }, glow: { r: 0, g: 0, b: 0 } }),
+    // new Triangle([new Vector(3, 7, 10), new Vector(-3, 7, 10), new Vector(3, 1, 10)], { reflectivity: { r: 1, g: 0.5, b: 1 }, glow: { r: 0, g: 0, b: 0 } }),
+    // new Triangle([new Vector(-3, 1, 13), new Vector(3, 1, 13), new Vector(-3, 7, 13)], { reflectivity: { r: 0.5, g: 1, b: 1 }, glow: { r: 0, g: 0, b: 0 } }),
+    // new Triangle([new Vector(3, 7, 13), new Vector(-3, 7, 13), new Vector(3, 1, 13)], { reflectivity: { r: 0.5, g: 1, b: 1 }, glow: { r: 0, g: 0, b: 0 } }),
+    // new Triangle([new Vector(-3, 1, 10), new Vector(-3, 1, 13), new Vector(3, 1, 13)], { reflectivity: { r: 1, g: 1, b: 0.5 }, glow: { r: 0, g: 0, b: 0 } }),
+    // new Triangle([new Vector(3, 1, 10), new Vector(-3, 1, 10), new Vector(3, 1, 13)], { reflectivity: { r: 1, g: 1, b: 0.5 }, glow: { r: 0, g: 0, b: 0 } }),
+    // new Sphere(0.1, new Vector(4, 0, 7), { reflectivity: { r: 1, g: 1, b: 1 }, glow: { r: 255, g: 255, b: 255 } })
+    new Triangle([new Vector(-10,-10,-10), new Vector(-10,10,-10), new Vector(10,-10,-10)], {reflectivity: {r: 1, g: 1, b: 1}, glow: {r:0,g:0,b:0}}),
+    new Triangle([new Vector(10,-10,-10), new Vector(10,10,-10), new Vector(-10,10,-10)], {reflectivity: {r: 1, g: 1, b: 1}, glow: {r:0,g:0,b:0}}),
+    new Triangle([new Vector(-10,-10,10), new Vector(-10,10,10), new Vector(10,-10,10)], {reflectivity: {r: 1, g: 1, b: 1}, glow: {r:0,g:0,b:0}}),
+    new Triangle([new Vector(10,-10,10), new Vector(10,10,10), new Vector(-10,10,-10)], {reflectivity: {r: 1, g: 1, b: 1}, glow: {r:0,g:0,b:0}}),
+    new Sphere(0.1, new Vector(0,0,6), {reflectivity: {r:1, g:1, b:1}, glow: {r:255,b:255,g:255}})
 ])
 
 
-let width = 1000
-let height = 1000
+let width = 500
+let height = 500
 
 ctx.canvas.width = width
 ctx.canvas.height = height
 
 ctx.fillStyle = "rgb(0,0,0)"
-ctx.fillRect(0,0,width,height)
+ctx.fillRect(0, 0, width, height)
 
 let fieldOfView = 50 * width / 100
 
 let samples = 100
 
-let pixelGrid = Array(width).fill(0).map(() => new Array(height).fill(0).map(() => {return {r:0,g:0,b:0}}))
+let pixelGrid = Array(width).fill(0).map(() => new Array(height).fill(0).map(() => { return { r: 0, g: 0, b: 0 } }))
 
-function trace(ray,depth,currentObj){
-    if(depth <= 0){return {r:0,g:0,b:0}}
+function trace(ray, depth, currentObj) {
+    if (depth <= 0) { return { r: 0, g: 0, b: 0 } }
     results = world.objects.filter(o => o != currentObj).map(o => o.collision(ray))
     let result = results
-    .filter(a=>a.collide)
-    .reduce((a,b)=>{
-        return a.dist < b.dist ? a : b
-    },{collide:false,dist:Infinity})
-    
-    if(!result.collide){return {r:0,g:0,b:0}}
-    
+        .filter(a => a.collide)
+        .reduce((a, b) => {
+            return a.dist < b.dist ? a : b
+        }, { collide: false, dist: Infinity })
+
+    if (!result.collide) { return { r: 0, g: 0, b: 0 } }
+
     let glow = result.obj.properties.glow
-    
+
     let reflectivity = result.obj.properties.reflectivity
-    
-    let reflection = trace(reflect(result.point,ray,result.normal),depth-1,result.obj)
-    
+
+    let reflection = trace(reflect(result.point, ray, result.normal), depth - 1, result.obj)
+
     return ({
-        r:glow.r + reflectivity.r * reflection.r, 
-        g:glow.g + reflectivity.g * reflection.g,
-        b:glow.b + reflectivity.b * reflection.b
+        r: glow.r + reflectivity.r * reflection.r,
+        g: glow.g + reflectivity.g * reflection.g,
+        b: glow.b + reflectivity.b * reflection.b
     })
 }
 
-function reflect(point,ray,normal){
+function reflect(point, ray, normal) {
     let roughness = 1
-    let newDirection = Vector.subtract(ray.direction, Vector.scale(normal, 2 * Vector.dot(ray.direction,normal)))
-    newDirection.x += (Math.random()-0.5)*roughness
-    newDirection.y += (Math.random()-0.5)*roughness
-    newDirection.z += (Math.random()-0.5)*roughness
-    return(new Ray(point,newDirection))
+    let newDirection = Vector.subtract(ray.direction, Vector.scale(normal, 2 * Vector.dot(ray.direction, normal)))
+    newDirection.x += (Math.random() - 0.5) * roughness
+    newDirection.y += (Math.random() - 0.5) * roughness
+    newDirection.z += (Math.random() - 0.5) * roughness
+    return (new Ray(point, newDirection))
 }
 
-function render(sample){
-    for(let i = 0; i < width; i++){
-        for(let j = 0; j < height; j++){
-            x = i-width/2
-            y = j-height/2
-            let ray = new Ray(new Vector(0,0,0), new Vector(x,y,fieldOfView).normalize())
-            color = trace(ray,10)
+function render(sample) {
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
+            x = i - width / 2
+            y = j - height / 2
+            let ray = new Ray(new Vector(0, 0, 0), new Vector(x, y, fieldOfView).normalize())
+            color = trace(ray, 10)
             pixelGrid[i][j].r += color.r / samples
             pixelGrid[i][j].g += color.g / samples
             pixelGrid[i][j].b += color.b / samples
         }
     }
-    paint(pixelGrid,sample)
+    paint(pixelGrid, sample)
 }
 
-function paint(pixels,sample){
-    for(let i = 0; i < width; i++){
-        for(let j = 0; j < height; j++){
+function paint(pixels, sample) {
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
             color = pixels[i][j]
-            colorScaleFactor = samples/(sample+1)
-            ctx.fillStyle = `rgb(${color.r*colorScaleFactor},${color.g*colorScaleFactor},${color.b*colorScaleFactor})`
-            ctx.fillRect(i,j,1,1)
+            colorScaleFactor = samples / (sample + 1)
+            ctx.fillStyle = `rgb(${color.r * colorScaleFactor},${color.g * colorScaleFactor},${color.b * colorScaleFactor})`
+            ctx.fillRect(i, j, 1, 1)
         }
     }
 }
 
 
-for(let i = 0; i<samples;i++){
-    setTimeout(()=>{render(i)},0)
+for (let i = 0; i < samples; i++) {
+    setTimeout(() => { render(i) }, 0)
 }
--- scene.js
+++ scene.js
@@ -1,37 +1,37 @@
 //3d objects library
 
 class World {
-    constructor(objects = []){
+    constructor(objects = []) {
         this.objects = objects
     }
 
-    add(object){
+    add(object) {
         this.objects.push(object)
     }
 
-    remove(object){
+    remove(object) {
         this.objects = this.objects.filter(o => o != object)
     }
 }
 
 class Shape {
-    constructor(properties){
+    constructor(properties) {
         this.properties = properties
     }
 }
 
 class Sphere extends Shape {
-    constructor(radius,location,properties){
+    constructor(radius, location, properties) {
         super(properties)
         this.radius = radius
         this.location = location
     }
 
-    collision(ray){
+    collision(ray) {
         //good article: http://kylehalladay.com/blog/tutorial/math/2013/12/24/Ray-Sphere-Intersection.html
-        let distRay = Vector.subtract(this.location,ray.origin)
+        let distRay = Vector.subtract(this.location, ray.origin)
         let distToCenter = distRay.magnitude
-        let rayDistToCenter = Vector.dot(distRay,ray.direction)
+        let rayDistToCenter = Vector.dot(distRay, ray.direction)
         let rayDistFromCenterSquared = distToCenter ** 2 - rayDistToCenter ** 2
 
         let radiusSquared = this.radius ** 2
@@ -39,112 +39,115 @@ class Sphere extends Shape {
         let distToSurface = rayDistToCenter - Math.sqrt(Math.abs(radiusSquared - rayDistFromCenterSquared))
 
         let collide = true
-        
-        if(rayDistToCenter < 0){ collide = false }
-        if(rayDistFromCenterSquared > radiusSquared){ collide = false }
 
-        if(!collide){distToSurface = Infinity}
+        if (rayDistToCenter < 0) { collide = false }
+        if (rayDistFromCenterSquared > radiusSquared) { collide = false }
 
-        let point =  Vector.add(Vector.scale(ray.direction,distToSurface),ray.origin)
+        if (!collide) { distToSurface = Infinity }
+
+        let point = Vector.add(Vector.scale(ray.direction, distToSurface), ray.origin)
         // let point =  Vector.scale(ray.direction,distToSurface)
-        
-        return {collide:collide, dist:distToSurface, point:point, normal:Vector.subtract(point,this.location).normalize(),obj:this}
+
+        return { collide: collide, dist: distToSurface, point: point, normal: Vector.subtract(point, this.location).normalize(), obj: this }
     }
 }
 
 class Triangle extends Shape {
-    constructor(points, properties){
+    constructor(points, properties) {
         super(properties)
         this.points = points
     }
 
-    collision(ray){
-        let planeVector = Vector.cross(Vector.subtract(this.points[1],this.points[0]),Vector.subtract(this.points[2],this.points[0])).normalize()
-        let planeOffset = Vector.dot(planeVector,this.points[0])
-        let distToSurface = (planeOffset - Vector.dot(planeVector, ray.origin))/Vector.dot(planeVector, ray.direction)
-        let point = Vector.add(Vector.scale(ray.direction,distToSurface),ray.origin)
-        let c1 = Vector.dot(Vector.cross(Vector.subtract(this.points[1],this.points[0]),Vector.subtract(point,this.points[0])),planeVector) >= 0
-        let c2 = Vector.dot(Vector.cross(Vector.subtract(this.points[2],this.points[1]),Vector.subtract(point,this.points[1])),planeVector) >= 0
-        let c3 = Vector.dot(Vector.cross(Vector.subtract(this.points[0],this.points[2]),Vector.subtract(point,this.points[2])),planeVector) >= 0
+    collision(ray) {
+        let planeVector = Vector.cross(Vector.subtract(this.points[1], this.points[0]), Vector.subtract(this.points[2], this.points[0])).normalize()
+        let planeOffset = Vector.dot(planeVector, this.points[0])
+        let distToSurface = (planeOffset - Vector.dot(planeVector, ray.origin)) / Vector.dot(planeVector, ray.direction)
+        let point = Vector.add(Vector.scale(ray.direction, distToSurface), ray.origin)
+        let c1 = Vector.dot(Vector.cross(Vector.subtract(this.points[1], this.points[0]), Vector.subtract(point, this.points[0])), planeVector) >= 0
+        let c2 = Vector.dot(Vector.cross(Vector.subtract(this.points[2], this.points[1]), Vector.subtract(point, this.points[1])), planeVector) >= 0
+        let c3 = Vector.dot(Vector.cross(Vector.subtract(this.points[0], this.points[2]), Vector.subtract(point, this.points[2])), planeVector) >= 0
         let collide = c1 && c2 && c3
 
-        if(!collide){distToSurface = Infinity}
+        if (!collide) { distToSurface = Infinity }
 
-        return {collide:collide, dist:distToSurface, point:point, normal:planeVector, obj:this}
-        
+        return { collide: collide, dist: distToSurface, point: point, normal: planeVector, obj: this }
     }
 }
 
-class Ray{
-    constructor(origin, direction){
+class Ray {
+    constructor(origin, direction) {
         this.origin = origin
         this.direction = direction
     }
 }
 
-class Vector{
-    constructor(x,y,z){
+class Vector {
+    constructor(x, y, z) {
         this.x = x
         this.y = y
         this.z = z
     }
 
-    get magnitude(){
-        return Math.sqrt(this.x**2 + this.y**2 + this.z**2)
+    get magnitude() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2)
     }
 
-    set magnitude(len){
+    set magnitude(len) {
         let scaleFactor = len / this.magnitude
         this.scale(scaleFactor)
     }
 
-    copy(){
+    copy() {
         return new Vector(this.x, this.y, this.z)
     }
 
-    normalize(){
+    normalize() {
         this.magnitude = 1
         return this
     }
 
-    scale(s){
+    scale(s) {
         this.x *= s
         this.y *= s
         this.z *= s
         return this
     }
 
-    add(v){
+    add(v) {
         this.x += v.x
         this.y += v.y
         this.z += v.z
         return this
     }
 
-    subtract(v){
+    subtract(v) {
         this.x -= v.x
         this.y -= v.y
         this.z -= v.z
         return this
     }
 
-    static add(v1,v2){
+    static normalize(v) {
+        return v.copy().normalize()
+    }
+
+    static add(v1, v2) {
         return new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z)
     }
 
-    static subtract(v1,v2){
+    static subtract(v1, v2) {
         return new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z)
     }
 
-    static scale(v, s){
+    static scale(v, s) {
         return new Vector(v.x * s, v.y * s, v.z * s)
     }
 
-    static dot(v1, v2){
+    static dot(v1, v2) {
         return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
     }
 
-    static cross(v1, v2){
+    static cross(v1, v2) {
         return new Vector(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x)
     }
 }
